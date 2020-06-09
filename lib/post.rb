e7c014b65dadc6ea92daf9f3e9f4f27c3677d738
mcgillshakore <mcgills2093@gmail.com>
2020-06-08 22:37:40 -0500

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,15 @@
+class Post
+    def title
+        @title
+    end
+    def title=(title)
+        @title = title
+    end
+    def author
+        @author
+    end
+
+    def author=(author)
+        @author = author
+    end
+end
---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+class Song
+    def title
+        @title
+    end
+
+    def title=(title)
+        @title = title
+    end
+
+    def artist
+        @artist
+    end
+
+    def artist=(artist)
+        @artist = artist
+    end
+end
